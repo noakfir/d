8d456d8c0ae52d53c79dd8166da1a61ce84d5fcc
noakfir <95751658+noakfir@users.noreply.github.com>
2022-11-22 15:43:09 +0200

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>API_URL: {window._env_.API_URL}</p>
-        <p>TEST: {process.env.TEST}</p>
+        <p>REACT_APP_TEST: {process.env.REACT_APP_TEST}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
